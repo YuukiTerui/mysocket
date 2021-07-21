bf1de2b05cc2ca25d1b51339a8540ea97c89dd04
YuukiTerui <teriyaki0839@gmail.com>
2021-07-21 15:07:35 +0900

    Create client.py

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import pickle
+from threading import Thread
+
+import const
+const.MSGLEN = 2**10
+
+class Client:
+    def __init__(self, host=None, port=None) -> None:
+        self.host = host
+        self.port = port
+        self.sock = None
+        self.send_log = []
+        self.receive_log = []
+
+    def connect_port(self, port=None):
+        if port is None:
+            port = self.port
+        if self.sock:
+            self.sock.close()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, port))
+
+    def send(self, msg):
+        if not self.sock:
+            return -1
+        msg = pickle.dumps(msg)
+        self.sock.send(msg)
+        
+        
+
+        #msg = self.sock.recv(const.MSGLEN)
+        #msg = pickle.loads(msg)
+        
